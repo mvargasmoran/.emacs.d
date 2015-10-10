d8a47786421548d1079f5982bdf285bf0471eaa9
mvargasmoran <mvargasmoran@gmail.com>
2015-10-10 18:52:25 -0300

    adding a load directory function to clean even more the init.el no longer using a bunch of (load "~/...")

---
+++ user-conf/neotree-config.el
@@ -0,0 +1,13 @@
+;; Neotree stuff
+(defun neotree-ffip-project-dir ()
+  "Open NeoTree using the git root."
+  (interactive)
+  (let ((project-dir (ffip-project-root))
+		(file-name (buffer-file-name)))
+	(if project-dir
+		(progn
+		  (neotree-dir project-dir)
+		  (neotree-find file-name))
+	  (message "Could not find git project root."))))
+
+(global-set-key (kbd "C-c C-p") 'neotree-ffip-project-dir)

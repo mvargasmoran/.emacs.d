35f1b92e882b6f57c1645507c24e0aa3cf216519
mvargasmoran <mvargasmoran@gmail.com>
2015-10-29 21:05:17 -0300

    making sure diminish conf is loaded last

--- user-conf/diminish-config.el
+++
@@ -1,4 +0,0 @@
-;;several stuff to diminish
-'(diminish 'git-gutter-mode)
-'(diminish 'helm-mode)
-'(diminish 'auto-complete-mode)
---
+++ user-conf/z-diminish-config.el
@@ -0,0 +1,10 @@
+;;several stuff to diminish
+
+;; Diminish modeline clutter
+;;(require 'diminish)
+(diminish 'git-gutter-mode)
+(diminish 'yas/minor-mode)
+(diminish 'auto-complete-mode)
+;; (diminish 'git-gutter-mode
+;; 		  'helm-mode
+;; 		  'auto-complete-mode)
